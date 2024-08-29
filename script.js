21f7e56c01bffddd4282bef9cd3c4402d833d5bb
kristianachwan <kristianachwan@gmail.com>
2024-08-29 17:18:14 +0800

    Add explanation for JS file

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const addButton = document.querySelector(".todo-button");
 
 const addTodo = () => {
+  // Mendapatkan todo input element
   const todoInput = document.querySelector(".todo-input");
+
+  // Mendapatkan parent container dari semua todo-list
   const todoList = document.querySelector(".todo-list");
+
+  // Mendapatkan value dari apa yang diisi oleh user
   const todoInputValue = todoInput.value;
+
+  // Membuat element <li>{todoInputValue}</li>
   const newTodo = document.createElement("li");
   newTodo.innerText = todoInputValue;
 
+  // Memasukan todo yang baru ke dalam container
   todoList.appendChild(newTodo);
 
+  // Mengosongkan input value agar user bisa menambahkan todo kembali tanpa harus menghapus
   todoInput.value = "";
 };
 
